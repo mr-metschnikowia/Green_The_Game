2a01ea33d97ae69d63f77cdbe3b3e0efe09c9f1b
mr-metschnikowia <gorbunov.lev@googlemail.com>
2021-01-21 22:51:30 +0000

    GPG integration + build 0.1

---
+++ Assets/GPGSAuthentication.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GPGSAuthentication : MonoBehaviour
+{
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+            PlayGamesPlatform.InitializeInstance(config);
+
+            PlayGamesPlatform.DebugLogEnabled = true;
+
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        // initialize GPGS instance - allows for communication with GPGS?
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Logged in successfully");
+            }
+            else
+            {
+                Debug.Log("FAILED TO LOGIN :(");
+            }
+        });
+        
+        // Login to GPSG instance
+    }
+
+    
+}
